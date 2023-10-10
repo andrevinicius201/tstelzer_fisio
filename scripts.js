47439edb6eee8ce41c282b2abf1bef6120e80476
andrevinicius201 <andre.vinicius1899@gmail.com>
2023-10-09 21:02:16 -0300

    new color pallet

--- scripts.js
+++ scripts.js
@@ -5,24 +5,24 @@ const card = document.querySelector("div.card");
 
 const videos = [
   {
-    title: "Aplicação Botox",
+    title: "Aplicação de botox",
     duration: "Dona Cleitinha",
     // thumb: "https://i.ytimg.com/vi/GykTLqODQuU/hqdefault.jpg",
-    thumb: "/images/botox-1.jpg",
+    thumb: "images/botox-1.jpg",
     // video_id: "GykTLqODQuU"
   },
   {
-    title: "Aplicação Botox",
+    title: "Aplicação de botox",
     duration: "Dona Sivalda",
     // thumb: "https://i.ytimg.com/vi/GykTLqODQuU/hqdefault.jpg",
-    thumb: "/images/botox-2.jpg",
+    thumb: "images/botox-2.jpg",
     // video_id: "GykTLqODQuU"
   },
   {
-    title: "Gatinha Xuxu",
+    title: "Aplicação de botox",
     duration: "Namorada do André",
     // thumb: "https://i.ytimg.com/vi/GykTLqODQuU/hqdefault.jpg",
-    thumb: "/images/ts_fisio.jpg",
+    thumb: "images/ts_fisio.jpg",
     // video_id: "GykTLqODQuU"
   },
 ];
@@ -32,36 +32,13 @@ videos.map(video => {
   cardClone.setAttribute("id", video.video_id);
   cardClone.querySelector("img").src = video.thumb;
   cardClone.querySelector(".title").innerHTML = video.title;
-  cardClone.querySelector(".info > p.text--medium").innerHTML =
-    video.duration;
+  // cardClone.querySelector(".title").innerHTML = "";
+  // cardClone.querySelector(".info > p.text--medium").innerHTML =
+  //   video.duration;
   sectionCards.appendChild(cardClone);
 });
 
 card.remove();
-
-// Modal actions
-const modalOverlay = document.querySelector(".modal-overlay");
-const modal = document.querySelector(".modal");
-const cards = [...document.querySelectorAll(".cards .card")];
-
-cards.forEach(card => {
-  card.addEventListener("click", () => {
-    modal.querySelector(
-      "iframe"
-    ).src = `https://www.youtube.com/embed/${card.getAttribute("id")}`;
-    modalOverlay.classList.add("active");
-    modal.classList.add("active");
-    document.querySelector("body").style.overflow = "hidden";
-  });
-});
-
-document.querySelector(".close-modal").addEventListener("click", () => {
-  modalOverlay.classList.remove("active");
-  modal.classList.remove("active");
-  modal.querySelector("iframe").src = ``;
-  document.querySelector("body").style.overflow = "initial";
-});
-
 
 
 
